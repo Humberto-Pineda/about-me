afb354ea3652453ff2fa356f9bc0dee7ab70a1fa
Carlos A. Herrera <cherrera1208@gmail.com>
2022-07-14 14:34:17 -0700

    added function guessAge

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 'use strict';
 
-// alert('Oi! How you doin?');
+alert('Oi! How you doin?');
 
 function yourName() {
   let yourName = prompt('I goes by Bert. What\'s your name?');
   console.log(yourName);
 }
-// yourName();
+yourName();
 
 function littleGame() {
   let playGame = prompt(yourName + '? Well it\'s nice to meet ya. I wanna play a lil game. A yes or no game. Play along? Waddaya say? Y or N').toLowerCase();
@@ -17,7 +17,7 @@ function littleGame() {
     alert('That\'s the wrong answer. You didn\'t have a choice, heh heh. Let\'s start.');
   }
 }
-// littleGame();
+littleGame();
 
 function bornYes() {
   let quest1 = prompt('Was I, in fact, born yesterday?').toLowerCase();
@@ -71,16 +71,19 @@ alert('You\'ll have 4 guesses for this next question.');
 
 let guessLeft = 3;
 
-for (let i = guessLeft; i >= 0; i--) {
-  let guessName = prompt('How old am I between 30 and 35?');
-  if (guessName === '31') {
-    alert('Wow! You\'re pretty good at this!');
-    break;
-  } else if (guessName < '31') {
-    alert('Nope! You\'re too low and have ' + i + ' guesses left!');
-  } else if (guessName > '31') {
-    alert('Nope! You\'re too high and have ' + i + ' guesses left!');
-  } if (i === 0) {
-    alert('I\'m 31. Game Over! Loser!');
+function guessAge() {
+  for (let i = guessLeft; i >= 0; i--) {
+    let guessName = prompt('How old am I between 30 and 35?');
+    if (guessName === '31') {
+      alert('Wow! You\'re pretty good at this!');
+      break;
+    } else if (guessName < '31') {
+      alert('Nope! You\'re too low and have ' + i + ' guesses left!');
+    } else if (guessName > '31') {
+      alert('Nope! You\'re too high and have ' + i + ' guesses left!');
+    } if (i === 0) {
+      alert('I\'m 31. Game Over! Loser!');
+    }
   }
 }
+guessAge();
