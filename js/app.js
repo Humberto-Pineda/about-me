7f168a21f645f8665bda40be7ec1a7af4d46faa3
Humberto Pineda <bpineda7540@gmail.com>
2022-07-14 22:47:56 -0700

    adding an array

--- js/app.js
+++ js/app.js
@@ -52,18 +52,36 @@
 // alert('I\'m having fun! Are you having fun? Let\'s keep going!');
 // alert('You\'ll have 4 guesses for this next question.');
 
-let guessLeft = 3;
+// let guessLeft = 3;
 
-for (let i = guessLeft; i >= 0; i--) {
-  let guessName = prompt('How old am I between 30 and 35?');
-  if (guessName === '31'){
-    alert('Wow! You\'re pretty good at this!');
-    break;
-  } else if (guessName < '31'){
-    alert('Nope! You\'re too low and have ' + i + ' guesses left!');
-  } else if (guessName > '31'){
-    alert('Nope! You\'re too high and have ' + i + ' guesses left!');
-  } if (i === 0){
-    alert('I\'m 31. Game Over! Loser!');
+// for (let i = guessLeft; i >= 0; i--) {
+//   let guessName = prompt('How old am I between 30 and 35?');
+//   if (guessName === '31'){
+//     alert('Wow! You\'re pretty good at this!');
+//     break;
+//   } else if (guessName < '31'){
+//     alert('Nope! You\'re too low and have ' + i + ' guesses left!');
+//   } else if (guessName > '31'){
+//     alert('Nope! You\'re too high and have ' + i + ' guesses left!');
+//   } if (i === 0){
+//     alert('I\'m 31. Game Over! Loser!');
+//   }
+// }
+
+// alert('Let\'s do one last question. You\'ll have 6 attempts. You\'ll also be able to see your choices. Good luck!');
+
+alert('Who is my favorite superhero?');
+
+let lastQ = ['Superman, Batman, Ironman, Spiderman, Wonder Woman, Omniman, Radioactive Man'];
+// let lastA = ['Batman'];
+// let lastChance = 6;
+
+for (let i = 0; i < lastQ.length; i++) {
+  prompt(lastQ);
+  if (lastQ[i] === ['Batman']) {
+    console.log('Someone has been paying attention');
+  }
+  else if (lastQ[i] !=='Batman') {
+    console.log('...Are you... joking? Try harder...');
   }
 }
